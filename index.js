9827718d901b5ab5ae7346ce7e32d882bda22bfd
Potter <37401491+yannick20@users.noreply.github.com>
2023-01-01 12:22:38 +0100

    aws node app 1

---
+++ index.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import { createServer } from "http";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With,Content,Accept,Content-Type,Authorization'
+    );
+    res.setHeader(
+      'Access-Control-Allow-Methods',
+      'GET,POST,DELETE,PATCH,OPTIONS'
+    );
+    if (req.method === 'OPTIONS') {
+      return res.sendStatus(200);
+    }
+    next();
+  });
+
+app.get(`/`, function (req, res) {
+	res.status(200).json({msg: `It's a GET request.`});
+});
+
+const PORT = 4000;
+const httpServer = createServer(app);
+
+httpServer.listen(PORT, () => {
+    console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
+  });
